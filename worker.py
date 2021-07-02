b20861602b2cdf66ae3cfc10055e1cc49701d1a1
blbadger <blbadger10@gmail.com>
2021-07-01 23:44:04 -0400

    added redis URL

--- worker.py
+++ worker.py
@@ -5,7 +5,7 @@ from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL', 'redis://:p2bc366791c653b891068ce502577528e16e4d711bec7ff6a2ff1fede43d2abd5@ec2-3-231-65-84.compute-1.amazonaws.com:20529')
 
 conn = redis.from_url(redis_url)
 
